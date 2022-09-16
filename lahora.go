4997c1b62c282db1e1e322b86b491b7a69ed52d9
José Albornoz <jojo@eljojo.net>
2022-09-16 16:19:56 -0400

    simpler version

--- lahora.go
+++ lahora.go
@@ -13,7 +13,7 @@ var ntp_server = "192.168.90.56"
 func hello(w http.ResponseWriter, req *http.Request) {
 	response, _ := ntp.Query(ntp_server)
 	time := time.Now().Add(response.ClockOffset).Format(time.RFC3339Nano)
-	fmt.Fprintf(w, "time.eljojo.net\n%s\nNTP precision: %v\nNTP dispersion: %v", time, response.Precision, response.RootDispersion)
+	fmt.Fprintf(w, "%s", time)
 }
 
 func main() {
