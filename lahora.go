beeb65473224fb9da11339d906c383e4225c30c6
José Albornoz <jojo@eljojo.net>
2022-09-16 16:12:11 -0400

    small tweaks

--- lahora.go
+++ lahora.go
@@ -8,12 +8,12 @@ import (
 	"github.com/beevik/ntp"
 )
 
-var ntp_server = "time.eljojo.net"
+var ntp_server = "192.168.90.56"
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	response, _ := ntp.Query(ntp_server)
 	time := time.Now().Add(response.ClockOffset).Format(time.RFC3339Nano)
-	fmt.Fprintf(w, "%s\n%v\nprecision: %v", ntp_server, time, response.Precision)
+	fmt.Fprintf(w, "time.eljojo.net\n%s\nNTP precision: %v\nNTP dispersion: %v", time, response.Precision, response.RootDispersion)
 }
 
 func main() {
